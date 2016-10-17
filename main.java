6384633a200fbe94cc7def2b33e50375a8c03b6e
viilupk <kadi.viilup@gmail.com>
2016-10-18 00:17:04 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Admin on 17.10.2016.
+ */
+public class main {
+    public static void main(String[] args) {
+        int a = 10;
+        while (a >= 1) {
+            System.out.println(a);
+            a--;
+        }
+
+        }
+}
