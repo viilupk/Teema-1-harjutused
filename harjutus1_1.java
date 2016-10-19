502185d67a7390dcc8c2f642c18aba7f635c1af9
viilupk <kadi.viilup@gmail.com>
2016-10-19 20:00:52 +0300

    Add files via upload

---
+++ harjutus1_1.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Admin on 19.10.2016.
+ */
+public class harjutus1_1 {
+    public static void main(String[] args) {
+        int a = 10;
+        while (a >= 1) {
+            System.out.println(a);
+            a--;
+        }
+
+    }
+
+}
