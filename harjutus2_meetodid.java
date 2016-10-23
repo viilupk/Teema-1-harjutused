0a7436fb10adb99db0737e5e03655d6ca7aa058a
viilupk <kadi.viilup@gmail.com>
2016-10-23 19:49:46 +0300

    Add files via upload

---
+++ harjutus2_meetodid.java
@@ -0,0 +1,23 @@
+import sun.font.TrueTypeFont;
+
+import java.util.Scanner;
+
+/**
+ * Created by Admin on 23.10.2016.
+ */
+public class harjutus2_meetodid {
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
